05378a8bab87cd2f6abed13099c7560043a83d7b
SoaB1 <kumachan1230@gmail.com>
2022-10-14 04:03:38 +0900

    changes to tree command.

--- cog/commands.py
+++ cog/commands.py
@@ -1,21 +1,20 @@
 import discord
 import os
 from discord.ext import commands
+from discord import app_commands
 from src import spreadsheet
 
-class BasicCog(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot: commands.Bot = bot
-    
-    @commands.command()
-    # async def test(self,ctx):
-        # await ctx.send("test!")
-    async def ticket(self, ctx):
+class ticket(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @app_commands.command(name='ticket', description='Create a new ticket')
+    async def ticket(self, interaction: discord.Interaction):
         ticketNum = await spreadsheet.getTicketNumber()
         embed = discord.Embed(title='New Ticket', description='Please fill out the following form, information to create a new ticket.', color=0x00ff00)
         embed.add_field(name='Ticket Number', value=ticketNum, inline=False)
         embed.add_field(name='URL', value=os.environ['FORM_URL'])
-        await ctx.send(embed=embed)
-
+        await interaction.response.send_message(embed=embed)
+    
 async def setup(bot: commands.Bot):
-    await bot.add_cog(BasicCog(bot))
+    await bot.add_cog(ticket(bot))
