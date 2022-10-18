cb5c6b4086056be203b0f6d53419d4ed6ea2e31a
SoaB1 <kumachan1230@gmail.com>
2022-10-18 19:01:56 +0900

    change for logger.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Load a Library for the bot
-from cmath import log
-import os, dotenv, discord, asyncio, logging
+from asyncio.log import logger
+import os, dotenv, discord, asyncio
+import logging
+import logging.handlers
 from discord.ext import commands
 from dotenv import load_dotenv
+
+# import formatter
 load_dotenv()
 
 COGS = [
@@ -19,12 +23,26 @@ class MyBot(commands.Bot):
         print(f'Logged in as {self.user}#{self.user.id}')
 
 async def main():
+    
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+    
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        mode='w',
+        maxBytes=100000,
+        backupCount=5
+    )
+    date_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', datefmt=date_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     bot = MyBot(intents=discord.Intents.all(), prefix='/')
     for cog in COGS:
         await bot.load_extension(cog)
         
-    # await bot.start(os.environ['TOKEN'])
-    await bot.start(os.environ['TOKEN'], log_handler=handler)
+    await bot.start(os.environ['TOKEN'])
 
 if __name__ == '__main__':
     asyncio.run(main())
