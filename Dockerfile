d2bd743831fa2ae936558b29ef40edc41959dc34
SoaB1 <kumachan1230@gmail.com>
2022-10-22 15:27:50 +0900

    Change for GCP SDK.

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,23 @@
-FROM python:3.10.7-alpine
+FROM python:3.10.8-bullseye
 
-RUN apk update && \
-    apk upgrade && \
-    apk add --no-cache \
-    jq
+FROM gcr.io/google.com/cloudsdktool/google-cloud-cli:slim
 
-RUN pip install --upgrade pip
-WORKDIR /root/dsc-bot
-
-COPY requirements.txt /root/dsc-bot
-COPY main.py /root/dsc-bot
-COPY cog/ /root/dsc-bot/cog
-COPY src/ /root/dsc-bot/src
-COPY ./entrypoint.sh /entrypoint.sh
+WORKDIR /cloud-bot
+COPY main.py /cloud-bot/main.py
+COPY src /cloud-bot/src
+COPY cog /cloud-bot/cog
+COPY requirements.txt /cloud-bot/requirements.txt
+COPY entrypoint.sh /entrypoint.sh
 
 RUN pip install -r requirements.txt
+RUN cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+RUN apt-get update && apt-get install -y \
+    curl \
+    jq \
+    && rm -rf /var/lib/apt/lists/*
 
 ENV TOKEN='TH1515N0TT0KEN' \
     SHEET_URL='https://docs.google.com/spreadsheets/' \
     FORM_URL='https://docs.google.com/forms/'
 
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 # shellcheck shell=dash
 
 set -e
@@ -24,22 +24,46 @@ logErr() {
     echo "[${LOGDATE}][ERROR] $1"
 }
 
-JSON_FILE=$(find /root/dsc-bot/ -maxdepth 1 -type f -name "*.json")
+authAccount() {
+    local account_name="$1"
+    local account_key="$2"
 
-if [ -f "${JSON_FILE}" ]; then
-    JSON_TYPE=$(jq -r '.type' <"${JSON_FILE}")
+    if [ "$(gcloud auth list --filter=status:ACTIVE --format="value(account)")" != "${account_name}" ]; then
+        logInfo "Authenticating Service Account..."
+        gcloud auth activate-service-account "${account_name}" --key-file="${account_key}"
+        if [ "$(gcloud auth list --filter=status:ACTIVE --format="value(account)")" = "${account_name}" ]; then
+            logInfo "Service Account authenticated."
+        else
+            logErr "Service Account authentication failed."
+            exit 1
+        fi
+    else
+        logInfo "Service Account already authenticated."
+    fi
+}
+
+JSON_PATH=$(find /cloud-bot/ -maxdepth 1 -type f -name "*.json")
+
+if [ -f "${JSON_PATH}" ]; then
+    JSON_TYPE=$(jq -r '.type' <"${JSON_PATH}")
     if [ "${JSON_TYPE}" != "service_account" ]; then
         logErr "This is not a service account file."
-        logWarn "FILE: ${JSON_FILE}"
+        logWarn "FILE: ${JSON_PATH}"
         exit 2
     else
         logInfo "Service account file found."
-        logInfo "FILE: ${JSON_FILE}"
+        logInfo "FILE: ${JSON_PATH}"
+        GOOGLE_APPLICATION_CREDENTIALS="${JSON_PATH}"
+        SERVICE_ACCOUNT_NAME=$(jq -r '.client_email' <"${JSON_PATH}")
+        PROJECT_ID=$(jq -r '.project_id' <"${JSON_PATH}")
+        export GOOGLE_APPLICATION_CREDENTIALS
+        export SERVICE_ACCOUNT_NAME
+        export PROJECT_ID
+        authAccount "${SERVICE_ACCOUNT_NAME}" "${GOOGLE_APPLICATION_CREDENTIALS}"
     fi
+    logInfo "Starting build-cloud-bot..."
+    python3 /cloud-bot/main.py
 else
     logErr "No service account file found."
     exit 1
 fi
-
-logInfo "Starting DSC Bot..."
-python3 /root/dsc-bot/main.py
