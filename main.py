6258e263d65ae6a167ac39f5b8a07f4a58073f1d
SoaB1 <kumachan1230@gmail.com>
2022-10-22 15:18:16 +0900

    change logger handler

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import logging.handlers
 from discord.ext import commands
 from dotenv import load_dotenv
+from logging import INFO, DEBUG, NOTSET
 
 # import formatter
 load_dotenv()
@@ -13,31 +14,35 @@ COGS = [
     'cog.commands'
 ]
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-
 class MyBot(commands.Bot):
     def __init__(self, prefix: str, intents: discord.Intents):
         super().__init__(command_prefix=prefix, intents=intents)
     
     async def on_ready(self):
-        print(f'Logged in as {self.user}#{self.user.id}')
+        logger.info(f'Logged in as {self.user}#{self.user.id}')
 
 async def main():
     
+    date_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname}] {name}: {message}', datefmt=date_fmt, style='{')
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
     
-    handler = logging.handlers.RotatingFileHandler(
+    file_handler = logging.handlers.RotatingFileHandler(
         filename='discord.log',
         encoding='utf-8',
         mode='w',
         maxBytes=100000,
         backupCount=5
     )
-    date_fmt = '%Y-%m-%d %H:%M:%S'
-    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', datefmt=date_fmt, style='{')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    file_handler.setFormatter(formatter)
+    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    
+    logging.basicConfig(level=NOTSET, handlers=[stream_handler, file_handler])
+    
     bot = MyBot(intents=discord.Intents.all(), prefix='/')
     for cog in COGS:
         await bot.load_extension(cog)
